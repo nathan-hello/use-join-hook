5120d8f98ca10d5d5d690ff36dca8cf9feb54b30
Nathan Smith <nathan.smith@synergyecp.com>
2024-12-02 18:14:05 -0500

    npm publish with types

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ outDir: "dist", entryRoot: "src", insertTypesEntry: true }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "use-join",
       fileName: (format) => `index.${format}.js`,
-      formats: ["es"],
+      formats: ["es", "umd"],
     },
     rollupOptions: {
       external: ["react", "react-dom"], // Don't bundle React
